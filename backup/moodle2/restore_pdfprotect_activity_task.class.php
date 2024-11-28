847dd20fc7a468ab19d337bacca34993641738b1
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-28 11:03:47 -0300

    Code checker

--- backup/moodle2/restore_pdfprotect_activity_task.class.php
+++ backup/moodle2/restore_pdfprotect_activity_task.class.php
@@ -22,7 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
+defined("MOODLE_INTERNAL") || die;
 require_once("{$CFG->dirroot}/mod/pdfprotect/backup/moodle2/restore_pdfprotect_stepslib.php");
 
 /**
