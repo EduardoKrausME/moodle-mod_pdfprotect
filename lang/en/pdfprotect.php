27c62e40adda2927aa0bf5c4a4931ae9b6cfb690
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-28 10:59:07 -0300

    First Commit

---
+++ lang/en/pdfprotect.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang EN file.
+ *
+ * @package   mod_pdfprotect
+ * @copyright 2024 Eduardo kraus (http://eduardokraus.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$string['contentheader'] = 'Conteúdo';
+$string['dnduploadpdfprotect'] = 'Criar novo PDF Protect';
+$string['filenotfound'] = 'Arquivo não localizado!';
+$string['modulename'] = 'PDF Protect';
+$string['modulename_help'] = 'O módulo de PDF Protect fornece um sistema altamente seguro contra download de PDF no Moodle';
+$string['modulenameplural'] = 'PDF Protects';
+$string['pdfprotect:addinstance'] = '';
+$string['pdfprotect:view'] = '';
+$string['pluginadministration'] = 'Administração do PDF Protect';
+$string['pluginname'] = 'PDF Protect';
+$string['privacy:metadata'] = 'The PDF Protect plugin does not store any personal data.';
+$string['resource:addinstance'] = 'Adicionar novo recurso';
+$string['resource:exportresource'] = 'Exportar PDF Protect';
+$string['resource:view'] = 'Ver PDF Protect';
+$string['resourcedetails_sizetype'] = '{$a->size} {$a->type}';
+$string['selectmainfile'] = 'Por favor, selecione o arquivo principal, clicando no ícone ao lado do nome do arquivo.';
