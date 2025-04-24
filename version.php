56667c9c38ebb9083101b162894308b1ea4f4bdf
Eduardo Kraus <kraus@eduardokraus.com>
2025-04-24 13:27:41 -0300

    V 3.2.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025031900;
+$plugin->version = 2025042400;
 $plugin->requires = 2019052000;
-$plugin->release = '3.2.2';
+$plugin->release = '3.2.1';
 $plugin->component = 'mod_pdfprotect';
 $plugin->maturity = MATURITY_STABLE;
